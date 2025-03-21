33a844b1680c1e853a4d436e241ca597e97cd782
Micah1001 <167143423+Micah1001@users.noreply.github.com>
2025-03-21 00:42:07 -0400

    Delete Scripts directory

--- Scripts/scripts.js
+++
@@ -1,6 +0,0 @@
-const menuToggle = document.querySelector('.menu-toggle');
-const navMenu = document.querySelector('nav ul');
-
-menuToggle.addEventListener('click', () => {
-    navMenu.classList.toggle('visible');
-});
